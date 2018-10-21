a190caa0e0bb724a9a0a1e35deff2d21d6e8d342
jrybojad <jrybojad@ucsc.edu>
2018-10-21 15:57:40 -0700

    Work on module.export & scope of reading function

--- webApp/server_modules/course_json_parser.js
+++ webApp/server_modules/course_json_parser.js
@@ -1,50 +1,51 @@
 const fs = require('fs');
-let coursesObjArray;
+var classObjArray;
 
 fs.readFile('..\\..\\UCSC_Custom_API\\output.json', (err, data) => {
   if (err){
 	  console.log("File does not exist");
 	  throw err;
   }
-  coursesObjArray = JSON.parse(data);
+  classObjArray = JSON.parse(data);
+  // The line below works only in the reading scope of this file.
+  console.log(getCourseName(classObjArray[1]));
 });
 
-module.export.courses = coursesObjArray;
+// Errors thrown for the line below:
+//module.export.courses = coursesObjArray;
 
-function getClassID(courseObject){
+function getClassID(classObject){
 	return classObject.ClassID;
 }
 
-function getClassFullName(courseObject) {
-	return classObject.CourseName;
+function getClassFullName(classObject) {
+	return classObject['Course Name'];
 }
 
-function splitFullName(courseObject){
+function splitFullName(classObject){
 	let fullNameString = "";
-	fullNameString += getClassFullName(courseObject);
+	fullNameString += getClassFullName(classObject);
 	return fullNameString.split(" ");
 }
 
 //Example: "Course Name": "AMS 3 - 01   Precalculus"
 
-function getCourseDept(courseObject){
-	let fullNameArray = splitFullName(courseObject);
+function getCourseDept(classObject){
+	let fullNameArray = splitFullName(classObject);
 	return fullNameArray[0];
 }
 
-function getCourseSection(courseObject){
-	let fullNameArray = splitFullName(courseObject);
+function getCourseSection(classObject){
+	let fullNameArray = splitFullName(classObject);
 	let courseSection = "";
 	courseSection = courseSection + fullNameArray[1] + fullNameArray[2] + fullNameArray[3];
 	return courseSection;
 }
 
-function getCourseName(courseObject) {
-	let fullNameArray = splitFullName(courseObject);
+function getCourseName(classObject) {
+	let fullNameArray = splitFullName(classObject);
 	let courseName = "";
-	courseName += fullNameArray[4];
+	courseName += fullNameArray[6];
 	return courseName;
 }
-
-//console.log(getCourseDept(coursesObjArray[0]));
 
