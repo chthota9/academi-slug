3dc215986339d6dce662a51265c647bed34c2a32
jrybojad <jrybojad@ucsc.edu>
2018-10-21 21:13:38 -0700

    OK, back to the normal

--- webApp/server_modules/course_json_parser.js
+++ webApp/server_modules/course_json_parser.js
@@ -1,51 +1,17 @@
 const fs = require('fs');
-var classObjArray;
+var classObject;
 
-fs.readFile('..\\..\\UCSC_Custom_API\\output.json', (err, data) => {
+fs.readFile('..\\..\\webScrapper\\ucsc-courses.json', (err, data) => {
   if (err){
 	  console.log("File does not exist");
 	  throw err;
   }
-  classObjArray = JSON.parse(data);
-  // The line below works only in the reading scope of this file.
-  console.log(getCourseName(classObjArray[1]));
+  multiClassObject = JSON.parse(data);
 });
 
 // Errors thrown for the line below:
 //module.export.courses = coursesObjArray;
 
-function getClassID(classObject){
-	return classObject.ClassID;
+function getClassID(className){
+	return multiClassObject[className];
 }
-
-function getClassFullName(classObject) {
-	return classObject['Course Name'];
-}
-
-function splitFullName(classObject){
-	let fullNameString = "";
-	fullNameString += getClassFullName(classObject);
-	return fullNameString.split(" ");
-}
-
-//Example: "Course Name": "AMS 3 - 01   Precalculus"
-
-function getCourseDept(classObject){
-	let fullNameArray = splitFullName(classObject);
-	return fullNameArray[0];
-}
-
-function getCourseSection(classObject){
-	let fullNameArray = splitFullName(classObject);
-	let courseSection = "";
-	courseSection = courseSection + fullNameArray[1] + fullNameArray[2] + fullNameArray[3];
-	return courseSection;
-}
-
-function getCourseName(classObject) {
-	let fullNameArray = splitFullName(classObject);
-	let courseName = "";
-	courseName += fullNameArray[6];
-	return courseName;
-}
-
