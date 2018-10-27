c24240ffb17254f1dec9d21ee99b41c1310e293c
Tim <tim.nguyen.cs@gmail.com>
2018-10-27 00:53:32 -0700

    Refactored navBar

---
+++ webApp/client/nav-buttons.js
@@ -0,0 +1,24 @@
+document.write('\
+<nav class="navbar navbar-expand-sm fixed-top">\
+        <div class="container-fluid">\
+            <div class="navbar-header">\
+                <a class="nav-brand" href="/profile/login"><img class="logo" src="https://cdn0.iconfinder.com/data/icons/insects-4/300/24-512.png" width="50" height="50" alt="Slug Icon">Academi-Slug</a>\
+            </div>\
+\
+            <ul class="nav navbar-nav navbar-right">\
+                <% if(!loggedIn) { %>\
+                    <li>\
+                        <a class="btn btn-nav" href="/profile/login" role="button">Log In</a>\
+                    </li>\
+                    <li>\
+                        <a class="btn btn-nav" href="/profile/signup" role="button">Sign Up</a>\
+                    </li>\
+                <% } else { %>\
+                    <li>\
+                        <a class="btn btn-nav" href="/profile" role="button">Profile</a>\
+                    </li>\
+                <% } %>\
+            </ul>\
+        </div>\
+    </nav>\
+');
