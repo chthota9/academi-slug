15d5dbca0cad498fd210c2655330aa6ff242301a
Tim <tim.nguyen.cs@gmail.com>
2018-10-29 13:09:44 -0700

    format changes

--- webApp/client/nav-buttons.js
+++ webApp/client/nav-buttons.js
@@ -2,7 +2,7 @@ document.write('\
 <nav class="navbar navbar-expand-sm fixed-top">\
         <div class="container-fluid">\
             <div class="navbar-header">\
-                <a class="nav-brand" href="/profile/login"><img class="logo" src="https://cdn0.iconfinder.com/data/icons/insects-4/300/24-512.png" width="50" height="50" alt="Slug Icon">Academi-Slug</a>\
+                <a class="nav-brand" href="/"><img class="logo" src="https://cdn0.iconfinder.com/data/icons/insects-4/300/24-512.png" width="50" height="50" alt="Slug Icon">Academi-Slug</a>\
             </div>\
 \
             <ul class="nav navbar-nav navbar-right">\
