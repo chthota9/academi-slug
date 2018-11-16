f97c05e22f758f821c83521a219dd81befec1179
Noel Gomez <nollgmz4@gmail.com>
2018-11-15 17:57:33 -0800

    buildtest

--- webApp/ecosystem.config.js
+++ webApp/ecosystem.config.js
@@ -17,6 +17,14 @@ module.exports = {
       GOOGLE_HAVEP_CALLBACK: 'https://academi-slug.herokuapp.com/google/login',
       GOOGLE_CREATEP_CALLBACK: 'https://academi-slug.herokuapp.com/google/signup',
       SECRET: 'slugger'
-    }
+    },
+    env_build: {
+      NODE_ENV: 'buildTest',
+      GOOGLE_CLIENT_ID: '675450206162-bo2tctf8cf17ba5mdgatgduho9cibi0e.apps.googleusercontent.com',
+      GOOGLE_CLIENT_SECRET: 'UaTfkdsuXOEHhYwauDs60VCU',
+      GOOGLE_HAVEP_CALLBACK: 'http://localhost:5000/google/login',
+      GOOGLE_CREATEP_CALLBACK: 'http://localhost:5000/google/signup',
+      SECRET: 'slugger'
+    },
   }]
 };
