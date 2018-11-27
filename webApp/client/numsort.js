ff1ec7e8cfc07394dd01d215a648dab60f5a505e
Tim <tim.nguyen.cs@gmail.com>
2018-11-27 10:03:02 -0800

    formatting

--- webApp/client/numsort.js
+++ webApp/client/numsort.js
@@ -6,18 +6,19 @@ function numSort() {
 
     while (switching) {
         switching = false;
-            for (i = 1; i < (rows.length - 1); i++) {
-                shouldSwitch = false;
-                x = rows[i].getElementsByTagName('td')[1];
-                y = rows[i + 1].getElementsByTagName('td')[1];
-                if (x.childElementCount < y.childElementCount) {
-                    shouldSwitch = true;
-                    break;
-                }
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName('td')[1];
+            y = rows[i + 1].getElementsByTagName('td')[1];
+            if (x.childElementCount < y.childElementCount) {
+                shouldSwitch = true;
+                break;
             }
+        }
+
         if (shouldSwitch) {
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                switching = true;
+            switching = true;
         }
     }
 }   
