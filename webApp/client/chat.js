a15dfab680a099aee71fe45b682c300a15c300d1
Noel Gomez <nollgmz4@gmail.com>
2018-11-29 22:32:12 -0800

    Start of new chat functionality

--- webApp/client/chat.js
+++ webApp/client/chat.js
@@ -4,12 +4,82 @@ var miniIcon = chatBox.querySelector('.minimize');
 var chatContainer = chatBox.querySelector('.chatContainer');
 var chatLinks = [...document.getElementsByClassName('.chat-link')];
 var chatNameTitle = chatBox.querySelector('.chatNameTitle');
+var dropDownArrow = chatNameTitle.querySelector('.dropDownArrow');
+var chatList = chatNameTitle.querySelector('.chatList');
 var minimized = true;
-var chattingWith = {};
-var currChat;
+var chatListMinimized = true;
+var chatInstances = {};
+var currChatInstance;
 // eslint-disable-next-line no-undef
 var socket = io();
 
+socket.on('recvPrivate', function(data) {
+    console.log(`${data.from.name} : msg ${data.msg}`);
+    let name = data.from.name;
+    let id = data.from.id;
+    chatNameTitle.textContent = name;
+    currChatInstance = id;
+    var node = document.createElement('div');
+    var textnode = document.createTextNode(data.msg);
+    node.classList.add('chat', 'recv');
+    node.appendChild(textnode);
+    chatContainer.appendChild(node);
+    node.scrollIntoView();
+    inputBox.disabled = false;
+});
+socket.on('chatSync', function(syncData) {
+    console.log(syncData);
+});
+
+function deleteChat () {
+    let newChat = chatContainer.cloneNode(false);
+    chatContainer.parentNode.replaceChild(newChat, chatContainer);
+    chatContainer = newChat;
+}
+
+function addChat (id) {
+    socket.emit('addChat', id, function(name) {
+        if (name) {
+            chatInstances[id] = name;
+            chatNameTitle.childNodes[0].textContent = name;
+            currChatInstance = id;
+            inputBox.disabled = false;
+            inputBox.focus();
+            if (minimized) {
+                changeChatBoxState();
+            }
+        }
+    });
+}
+
+changeCurrentChatInstance(id,name){
+    if(chatInstances[id] !== undefined){
+        currChatInstance = chatInstances[id].chatContainer;
+    }
+}
+
+miniIcon.addEventListener('click', changeChatBoxState);
+chatNameTitle.childNodes[0].addEventListener('click', dropDownMenu);
+dropDownArrow.addEventListener('click',dropDownMenu);
+
+function dropDownMenu () {
+    if (chatListMinimized) {
+        chatList.classList.remove('hidden');
+    }
+    else{
+        chatList.classList.add('hidden');
+    }
+    chatListMinimized = !chatListMinimized;
+}
+
+function changeChatBoxState () {
+    if (minimized) {
+        chatBox.classList.remove('minimized');
+    } else {
+        chatBox.classList.add('minimized');
+    }
+    minimized = !minimized;
+}
 chatLinks.forEach(link => {
     link.addEventListener('click', evt => evt.preventDefault());
 });
@@ -17,8 +87,8 @@ chatLinks.forEach(link => {
 inputBox.addEventListener('keydown', evt => {
     var key = evt.key || evt.keyCode;
     let msg = inputBox.value;
-    if (key === 'Enter' && currChat && msg !== '') {
-        socket.emit('sendPrivate', { msg , to: currChat });
+    if (key === 'Enter' && currChatInstance && msg !== '') {
+        socket.emit('sendPrivate', { msg, to: currChatInstance });
         var node = document.createElement('div');
         var textnode = document.createTextNode(msg);
         node.classList.add('chat', 'send');
@@ -29,50 +99,14 @@ inputBox.addEventListener('keydown', evt => {
     }
 });
 
-miniIcon.addEventListener('click', changeChatBoxState);
-chatNameTitle.addEventListener('click',changeChatBoxState);
-function changeChatBoxState () {
-    if (minimized) {
-        chatBox.classList.remove('minimized');
-        minimized = false;
-    } else {
-        chatBox.classList.add('minimized');
-        minimized = true;
-    }
+function ChatInstance(id,name) {
+    let chatContainer = document.createElement('div');
+    chatContainer.classList.add('chatContainer');
+    this.addToChat = function (newMsg) {
+        let node = document.createElement('div');
+        let textNode = document.createTextNode(newMsg);
+        node.classList.add('chat','send');
+        node.appendChild(textNode);
+        chatContainer.appendChild(node);
+    };
 }
-
-function addChat (id) {
-    socket.emit('addChat', id, function(name) {
-        if (name) {
-            chattingWith[id] = name;
-            chatNameTitle.textContent = name;
-            currChat = id;
-            inputBox.disabled = false;
-            inputBox.focus();
-            if(minimized){
-                changeChatBoxState();
-            }
-        }
-    });
-}
-
-socket.on('recvPrivate', function(data) {
-    console.log(`${data.from.name} : msg ${data.msg}`);
-    let name = data.from.name;
-    let id = data.from.id;
-    chatNameTitle.textContent = name;
-    currChat = id;
-    var node = document.createElement('div');
-    var textnode = document.createTextNode(data.msg);
-    node.classList.add('chat', 'recv');
-    node.appendChild(textnode);
-    chatContainer.appendChild(node);
-    node.scrollIntoView();
-    inputBox.disabled = false;
-});
-
-// socket.on('syncChats', function(data) {
-//     if (data) {
-//         chattingWith = data;
-//     }
-// });
--- webApp/server.js
+++ webApp/server.js
@@ -28,7 +28,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 // Establish home page
 app.get('/', function(req, res) {
-    console.log(req.session);
+    // console.log(req.session);
     let input = {
         loggedIn: req.isAuthenticated() && req.user.extra === undefined,
     };
